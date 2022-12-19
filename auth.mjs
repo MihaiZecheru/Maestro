6d4324f31522cea5ed1d46ca4b2f825d13d9a0dc
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-12-19 14:21:29 -0800

    del register func cus that will be done manually

--- auth.mjs
+++ auth.mjs
@@ -1,13 +1,6 @@
 import { getCookie, setCookie, deleteCookie } from './cookies.mjs';
 import API from './api.mjs';
 
-function uuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
 export function loggedIn() {
   return !!getCookie('token');
 }
@@ -25,9 +18,4 @@ export async function login(username, password) {
 
 export function logout() {
   deleteCookie('token');
-}
-
-export async function register(username, password) {
-  const token = uuid();
-  await API.post('users/' + username, { username, password, token });
 }
