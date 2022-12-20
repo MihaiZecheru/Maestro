f5c6d266a8eeb0d89c986358cb9c9649712773da
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-12-19 18:43:51 -0800

    finish /account/

--- account/index.js
+++ account/index.js
@@ -1,8 +1,43 @@
 import API from '../api.mjs';
 import { getUsername } from '../auth.mjs';
 
+let password;
+
 API.get('/users/' + getUsername()).then((user) => {
+  document.getElementById('username').innerHTML = user.username;
+  document.getElementById('password').value = user.password;
+  document.getElementById('pfp').src = user.pfp;
+  password = user.password;
+  
   if (user.isteacher) {
-    document.getElementById('stats').innerHTML = `<button class="btn btn-success">View Teacher Dashboard</button>`
+    document.getElementById('stats').innerHTML = `<a class="btn btn-success" href="/dashboard/">View Teacher Dashboard</a>`
+  } else {
+    document.getElementById('rank').innerHTML = user.rank;
+    document.getElementById('points').innerHTML = user.points;
+    document.getElementById('completed').innerHTML = user.completed;
+  }
+});
+
+document.getElementById('show-password').addEventListener('change', () => {
+  if (document.getElementById('show-password').checked) {
+    document.getElementById('password').type = 'text';
+  } else {
+    document.getElementById('password').type = 'password';
+  }
+});
+
+document.getElementById('show-password').addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') document.getElementById('show-password').click();
+});
+
+document.getElementById('password').addEventListener('keydown', (e) => {
+  const ele = document.getElementById('password');
+  const newPassword = ele.value;
+  console.log(e.key, newPassword)
+  if (e.key === 'Enter' && newPassword !== password) {
+    API.put(`/users/${getUsername()}/password`, newPassword).then(() => {
+      ele.value = password;
+      ele.blur();
+    });
   }
 });
