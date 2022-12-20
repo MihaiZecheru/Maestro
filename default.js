c9f8c0e9e9b96a8e1de62ad8d368a9f5b8ac70ba
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-12-19 17:04:35 -0800

    fixed typo / bug

--- default.js
+++ default.js
@@ -4,12 +4,16 @@ if (loggedIn()) {
   const login = document.getElementById('login');
   login.innerText = 'Account';
   login.href = '/account/';
+} else {
+  if (window.location.pathname !== '/' && window.location.pathname !== '/index.html' && window.location.pathname !== '/login/' && window.location.pathname !== '/login/index.html') {
+    window.location.href = '/';
+  }
 }
 
 document.querySelectorAll('button').forEach((button) => {
   button.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
-      e.target.children[0].click();
+      e.target.children[0]?.click();
     }
   });
 });
