7f997965104cd2819b6160cbefb562138db1402b
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-12-20 21:43:42 -0800

    finish /create/assignment/ route

---
+++ create/assignment/index.js
@@ -0,0 +1,148 @@
+import { teacherOnly } from '/auth.mjs';
+import API from '/api.mjs';
+
+teacherOnly();
+const nameBox = document.getElementById('name');
+const pointValueBox = document.getElementById('point-value');
+const moduleBox = document.getElementById('module');
+const description = document.getElementById('description');
+const submissionTypeBox = document.getElementById('submission-type');
+const allowCommentsBox = document.getElementById('allow-comments');
+const dateBox = document.getElementById('date');
+const submit = document.getElementById('submit');
+
+// disable past on datepicker
+new mdb.Datepicker(document.querySelector('.datepicker-disable-past'), {
+  disablePast: true
+});
+
+let modules
+API.getModules().then((_modules) => {
+  modules = _modules;
+});
+
+function setInvalid(e) {
+  e.target.classList.add('is-invalid');
+  e.target.classList.remove('is-valid');
+}
+
+function setValid(e) {
+  e.target.classList.add('is-valid');
+  e.target.classList.remove('is-invalid');
+}
+
+nameBox.addEventListener('input', (e) => {
+  if (/[^\w\s]/.test(e.target.value) || !e.target.value) {
+    setInvalid(e);
+  } else {
+    setValid(e);
+  }
+});
+
+pointValueBox.addEventListener('input', (e) => {
+  const v = e.target.value;
+
+  if (/[^\d\.]/.test(v) || (v.match(/\./g) || []).length > 1 || v.startsWith('.') || v.endsWith('.')) {
+    setInvalid(e);
+  } else {
+    setValid(e);
+  }
+});
+
+moduleBox.addEventListener('input', (e) => {
+  if (modules.includes(e.target.value)) {
+    setValid(e);
+  } else {
+    setInvalid(e);
+  }
+});
+
+description.addEventListener('input', (e) => {
+  if (e.target.value.length <= 500) {
+    document.getElementById('valid-charcount').innerText = e.target.value.length;
+    setValid(e);
+  } else {
+    document.getElementById('invalid-charcount').innerText = e.target.value.length;
+    setInvalid(e);
+  }
+});
+
+submissionTypeBox.addEventListener('change', (e) => {
+  if (e.target.value) {
+    setValid(e);
+  } else {
+    setInvalid(e);
+  }
+});
+
+allowCommentsBox.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    allowCommentsBox.click();
+  }
+});
+
+function dateboxHandler(e) {
+  if ((/^\d\d\/\d\d\/\d\d\d\d$/).test(e.target.value)) {
+    setValid(e);
+  } else {
+    setInvalid(e);
+  }
+}
+
+const datetimeBtn = document.querySelector('.far.fa-calendar.datepicker-toggle-icon');
+dateBox.addEventListener('input', dateboxHandler);
+dateBox.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    datetimeBtn.click();
+  }
+});
+
+datetimeBtn.addEventListener('click', () => {
+  (async () => {
+    setTimeout(() => {
+      document.querySelector('.datepicker-footer-btn.datepicker-ok-btn').addEventListener('click', () => {
+        dateboxHandler({ target: dateBox });
+      });
+    }, 100);
+  })();
+});
+
+submit.addEventListener('click', (e) => {
+  if (nameBox.classList.contains('is-invalid') || pointValueBox.classList.contains('is-invalid') || moduleBox.classList.contains('is-invalid') || description.classList.contains('is-invalid') || submissionTypeBox.classList.contains('is-invalid') || dateBox.classList.contains('is-invalid')) {
+    e.preventDefault();
+    return;
+  }
+
+  if (!nameBox.value || !pointValueBox.value || !moduleBox.value || !submissionTypeBox.value || !dateBox.value) {
+    // don't prevent default otherwise the tooltip won't show
+    return;
+  }
+
+  e.preventDefault();
+  API.createAssignment({
+    name: nameBox.value,
+    points: parseInt(pointValueBox.value),
+    module: moduleBox.value,
+    description: description.value,
+    submissionType: submissionTypeBox.value,
+    due: dateBox.value,
+    allowComments: allowCommentsBox.checked,
+  }).then((res) => {
+    if (res.status === 200) {
+      window.location.href = '/dashboard/';
+    } else {
+      new bootstrap.Modal(document.getElementById('error-modal')).show();
+    }
+  });
+});
+
+submit.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    submit.click();
+  }
+});
+
+nameBox.focus();
