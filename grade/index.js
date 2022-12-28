1e411b41c52c7f970b46b1bde8c54d30383c98b6
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-12-28 03:28:52 -0800

    base

--- grade/index.js
+++ grade/index.js
@@ -1,3 +1,12 @@
-const assignment_id = new URLSearchParams(window.location.search).get("assignment");
+import API from "../api.mjs";
+
+const assignment_number = new URLSearchParams(window.location.search).get("assignment");
 const module_id = new URLSearchParams(window.location.search).get("module");
 
+// const assignment_id // user dict here
+
+API.get(`/assignments/${module_id}/${assignment_id}/`).then((assignment) => {
+  API.get(`/submissions/assignments/${assignment_id}/`).then((submissions) => {
+    console.log(submissions);
+  });
+});
